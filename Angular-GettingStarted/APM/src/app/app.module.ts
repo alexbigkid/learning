4d86a29de5510c7c9b482aa508fa98c4d6a64678
Alex Berger <alexbigkid@users.noreply.github.com>
2018-01-07 23:24:39 -0800

    Module 8 completed. Nested Components. @Input and @Output decorators

--- Angular-GettingStarted/APM/src/app/app.module.ts
+++ Angular-GettingStarted/APM/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe
-  ],
   imports: [
     BrowserModule,
     FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   providers: [],
   bootstrap: [ AppComponent ]
--- Angular-GettingStarted/APM/src/app/products/product-list.component.ts
+++ Angular-GettingStarted/APM/src/app/products/product-list.component.ts
@@ -11,7 +11,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    
+
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -25,31 +25,30 @@ export class ProductListComponent implements OnInit {
     products: IProduct[] = [
         {
             productId: 2,
-            productName: "Garden Cart",
-            productCode: "GDN-0023",
-            releaseDate: "March 18, 2016",
-            description: "15 gallon capacity rolling garden cart",
+            productName: 'Garden Cart',
+            productCode: 'GDN-0023',
+            releaseDate: 'March 18, 2016',
+            description: '15 gallon capacity rolling garden cart',
             price: 32.99,
             starRating: 4.2,
-            imageUrl: "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            imageUrl: 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
         },
         {
             productId: 5,
-            productName: "Hammer",
-            productCode: "TBX-0048",
-            releaseDate: "May 21, 2016",
-            description: "Curved claw steel hammer",
+            productName: 'Hammer',
+            productCode: 'TBX-0048',
+            releaseDate: 'May 21, 2016',
+            description: 'Curved claw steel hammer',
             price: 8.9,
             starRating: 4.8,
-            imageUrl: "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+            imageUrl: 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
         }
     ];
 
     constructor() {
         this.filteredProducts = this.products;
-        this.listFilter = 'cart'
+        this.listFilter = 'cart';
     }
-
 
     toggleImage(): void {
         this.showImage = !this.showImage;
@@ -63,5 +62,9 @@ export class ProductListComponent implements OnInit {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
