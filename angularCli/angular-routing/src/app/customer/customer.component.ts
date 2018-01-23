82ca30548e84ab8c9a940a77fce58c8e046745e0
Alex Berger <alexbigkid@users.noreply.github.com>
2018-01-23 01:49:05 -0800

    added some code to test

--- angularCli/angular-routing/src/app/customer/customer.component.ts
+++ angularCli/angular-routing/src/app/customer/customer.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-
+  customers: any[];
+  hasPermissions = true;
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermissions) {
+      this.getCustomers()
+        .then(custmers => this.customers = custmers)
+        .catch(e => console.log(e.message));
+    } else {
+      this.customers = [];
+    }
+  }
+
+  async getCustomers() {
+    return [
+      {name: 'john', email: 'john@hunterindustries.com'},
+      {name: 'joanna', email: 'joanna@hunterindustries.com'}
+    ];
   }
 
 }
