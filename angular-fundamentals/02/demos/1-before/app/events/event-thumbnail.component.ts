f78b4fb0cd5ebc37c76b4ddc8c86bf92cd0bca80
Alex Berger <alexbigkid@users.noreply.github.com>
2018-01-26 12:59:49 -0800

    angularFundamentals:module3.5: handle Output from child to parent

--- angular-fundamentals/02/demos/1-before/app/events/event-thumbnail.component.ts
+++ angular-fundamentals/02/demos/1-before/app/events/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+// import { EventEmitter } from '@angular/core/src/facade/async';
 
 @Component ({
     selector: 'event-thumbnail',
@@ -13,9 +14,15 @@ import { Component, Input } from '@angular/core';
                 <span>&nbsp;</span>
                 <span>{{event.locatio.city}}, {{event.locatio.country}}</span>
             </div>
+            <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
         </div>
     `
 })
 export class EventThumbnailComponent {
     @Input() event: any
+    @Output() eventClick = new EventEmitter()
+
+    handleClickMe() {
+        this.eventClick.emit(this.event.name);
+    }
 }
